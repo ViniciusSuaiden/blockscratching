f35be9598a07e1cc5ee8677d6963822221926333
ViniciusSuaiden <vini.suaiden@gmail.com>
2022-09-29 14:15:31 -0300

    sdfhjk

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,3 +1,13 @@
-import { io } from "socket.io-client"
+import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+const socket = io('ws://localhost:3000')
 
-const socket = io("server:3000")
+socket.on("message", text => {
+  const el = document.createElement('li')
+  el.innerHTML = text
+  document.querySelector('ul').appendChild(el)
+})
+
+document.querySelector('button').onclick = () => {
+  const text = document.querySelector('input').value
+  socket.emit('message', text)
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,17 @@
 const http = require('http').createServer()
+const { Server } = require('socket.io')
 
-const io = require('socket.io')(http, {
+const io = new Server(http, {
     cors: {origin: "*"}
 })
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
     console.log('a user connected')
+
+    socket.on('message', message => {
+        console.log(message)
+        io.emit('message', `${socket.id.substring(0,2)} said ${message}`)
+    })
 })
 
-http.listen(3000, () => console.log('Server listening on port 3000'))
+http.listen(3000, () => console.log('Server listening on port 3000:3000'))
